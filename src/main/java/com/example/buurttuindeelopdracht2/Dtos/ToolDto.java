3143f94f92970fa8466dab43f30a0a7f47ad6a84
RJHARENBERG <reinardharenberg@gmail.com>
2022-05-27 09:56:07 +0200

    Dtos aan gemaakt voor de Tool en de User klassen zonder de relatie atrubuten

---
+++ src/main/java/com/example/buurttuindeelopdracht2/Dtos/ToolDto.java
@@ -0,0 +1,43 @@
+package com.example.buurttuindeelopdracht2.Dtos;
+
+public class ToolDto {
+
+    private Long id;
+
+    private String toolName;
+    private String type;
+    //    private IMG
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToolName() {
+        return toolName;
+    }
+
+    public void setToolName(String toolName) {
+        this.toolName = toolName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
