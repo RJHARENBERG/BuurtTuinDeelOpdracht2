24744cc38537647097f8d1ed93be355ca49fdcc8
RJHARENBERG <reinardharenberg@gmail.com>
2022-06-01 10:11:05 +0200

    map payload aan gemaakt met daar in de klassen AuthenticationRequest, AuthenticationResponse

---
+++ src/main/java/com/example/buurttuindeelopdracht2/Payload/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.buurttuindeelopdracht2.Payload;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
