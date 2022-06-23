3df93b06019d8138cb99f539ee61ded0e4f08ba4
RJHARENBERG <reinardharenberg@gmail.com>
2022-06-23 15:50:38 +0200

    admin classes verwijderd

--- src/main/java/com/example/buurttuindeelopdracht2/Repositorys/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.buurttuindeelopdracht2.Repositorys;
-
-import com.example.buurttuindeelopdracht2.Entiteiten.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
